eb7da36ab902ce0d2f4d85a2ffd20a9b59e60d31
Eric Butz <ericgbutz@gmail.com>
2023-06-21 15:13:00 -0600

    Update nav and add pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Truss Documentation',
+        title: 'Truss / Docs',
         logo: {
           alt: 'Truss Logo',
           src: 'img/logo.svg',
@@ -91,8 +91,12 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Truss Protocol',
+                to: '/truss',
+              },
+              {
+                label: 'Truss Marketplace',
+                to: '/marketplace',
               },
             ],
           },
@@ -100,16 +104,16 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                href: 'https://discord.com/invite/zerVhHtfxJ',
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/Truss_Security',
+              },
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/company/truss-security',
               },
             ],
           },
@@ -117,17 +121,17 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: 'Website',
+                href: 'https://www.truss-security.com',
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/truss-security',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Truss Security, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
--- src/pages/index.js
+++ src/pages/index.js
@@ -26,16 +26,24 @@ function HomepageHeader() {
   );
 }
 
+/*
+
+Add this back in to put features in homepage:
+
+      <main>
+        <HomepageFeatures />
+      </main>
+
+*/
+
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description="Truss documentation"
+      >
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
